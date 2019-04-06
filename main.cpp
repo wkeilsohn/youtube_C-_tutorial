e74535fd8738ffa2c2988d66be88af4124cc0ca4
William Keilsohn <kingwilliam42@gmail.com>
2019-04-06 11:32:48 -0400

    Array project
    Small project that deals with array type data structures.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int num1, num2;
-    char op;
+    int luckyNums[] = {4, 8, 15, 16, 23, 42};
 
-    cout << "Enter first number, please: ";
-    cin >> num1;
+    luckyNums[2] = 19;
 
-    cout << "Enter second number, please: ";
-    cin >> num2;
-
-    cout << "Enter operator, please: ";
-    cin >> op;
-
-    int result;
-
-    if(op == '+'){
-        result = num1 + num2;
-    } else if(op == '-'){
-        result = num1 - num2;
-    } else if(op == '*'){
-        result = num1 * num2;
-    } else if(op == '/'){
-        result = num1 / num2;
-    } else{
-    cout << "Invalid operator";
-    }
-    cout << "The result is "<< result;
+    cout << luckyNums[2];
 
     return 0;
 }
